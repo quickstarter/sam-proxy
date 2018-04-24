9ef12769558deaf987a11a8f69712711d4452c54
Sam Getlan <samgetlan@gmail.com>
2018-04-24 12:02:27 -0700

    Created a proxy server to patch together all group members modules

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
+
